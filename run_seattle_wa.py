12eed3e8790f149ed40fc1d1605208a49bb3f7a4
Angela Xu <xuhuanzhu@gmail.com>
2016-09-02 03:18:19 -0700

    run the scraper

--- run_seattle_wa.py
+++ run_seattle_wa.py
@@ -1,2 +1,15 @@
 from better_scraper_n import *
-a = get_skill_info('seattle', 'wa')
+
+# city = 'Seattle'
+# state = 'WA'
+city = 'Pittsburgh'
+state = 'PA'
+
+start_time = time.time()
+info = get_skill_info(city=city, state=state)
+print('--- %s seconds ---' % (time.time() - start_time))
+print('')
+
+print(info[1])
+info[0].savefig('/home/angelaxu/Xcode/projects/Seattle_090116.png')
+
