95f219401b92f695f61befcc39acb22a2323f1b6
Angela Xu <xuhuanzhu@gmail.com>
2016-09-06 13:30:08 -0700

    Program to run scraper

--- run_scraper.py
+++ run_scraper.py
@@ -10,23 +10,19 @@ city = args.city[0]
 state = args.state[0]
 
 start_time = job_scraper.time.time()
-info = job_scraper.run_scraper(city=city, state=state)
+(total_jobs_found, dataframe, figure) = job_scraper.run_scraper(city=city, state=state)
 run_time = job_scraper.time.time() - start_time
 
 today = job_scraper.datetime.date.today()
 today_1 = today.strftime('%m-%d-%Y')
 today_2 = today.strftime('%m_%d_%Y')
 
-start_time = jobscraper.time.time()
-(total_jobs_found, dataframe, figure) = jobscraper.run_scraper(city=city, state=state)
-run_time = jobscraper.time.time() - start_time
-
 print('')
-print('Date: ' + today)
+print('Date: ' + today_1)
 print('City: ' + city + ', ' + state)
 print('Number of Jobs Scraped: ' + str(total_jobs_found))
 print('Run Time: %s seconds' % (run_time))
 print('')
-print(info[df])
-dataframe.to_csv('output/%s_%s.txt' % (city, today), sep='\t')
-figure.savefig('output/%s_%s.png' % (city, today))
+print(dataframe)
+dataframe.to_csv('output/%s_%s.txt' % (city, today_2), sep='\t')
+figure.savefig('output/%s_%s.png' % (city, today_2))
